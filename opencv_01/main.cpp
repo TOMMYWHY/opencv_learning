205c4336221ff2ff480911fca55b80b6f0bc67df
tommy <tommywhy1989@gmail.com>
2020-02-02 22:11:33 +1300

    testing

--- opencv_01/main.cpp
+++ opencv_01/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "testing....~!!!!"<<endl;
+    Mat srcImage = imread("test01.jpeg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("[img]", srcImage);
+    waitKey(0);
     return 0;
 }

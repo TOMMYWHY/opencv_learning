41d421055e3c44aff5c2a743d0b5b476d1011d51
tommy <tommywhy1989@gmail.com>
2020-02-04 21:11:20 +1300

    ...

--- opencv_01/main.cpp
+++ opencv_01/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace cv;
 
+#if 0
 int main() {
     cout << "testing....~~~!!!!"<<endl;
     Mat srcImage = imread("test01.jpeg");
@@ -11,7 +12,21 @@ int main() {
         std::cout << "Image not loaded";
         return -1;
     }
-    imshow("[img]", srcImage);
+    imshow("demo1", srcImage);
+//    cout<< srcImage <<endl;
     waitKey(0);
     return 0;
 }
+
+#endif
+
+#if 1
+int main(){
+    Mat m = Mat(800,600,CV_8UC3,Scalar(0,0,255));
+    cout<< m <<endl;
+    imshow("testing",m);
+    waitKey(0);
+    return 0;
+}
+
+#endif

585c77e6e9d99857ecfffda01517a279d8d9fa1a
tommy <tommywhy1989@gmail.com>
2020-02-05 23:23:32 +1300

    01

--- opencv_01/main.cpp
+++ opencv_01/main.cpp
@@ -4,6 +4,33 @@
 using namespace std;
 using namespace cv;
 
+#if 1
+int main(){
+    Mat m = Mat(400,400,CV_8U,Scalar(0));
+    for (int col = 0; col < 400; col++) {
+        for (int row = 195; row < 205; row++) {
+            cout << (int)(*(m.data + m.step[0]*row+m.step[1]*col)) << "==> ";
+            *(m.data + m.step[0]*row + m.step[1]*col) =255;
+            cout << (int)(*(m.data + m.step[0]*row+m.step[1]*col)) << endl;
+
+        }
+    }
+    imshow("testing",m);
+    waitKey(0);
+    return 0;
+}
+#endif
+
+#if 0
+int main(){
+    Mat m = Mat(800,600,CV_8UC3,Scalar(0,0,255));
+    cout<< m <<endl;
+    imshow("testing",m);
+    waitKey(0);
+    return 0;
+}
+#endif
+
 #if 0
 int main() {
     cout << "testing..!!!!"<<endl;
@@ -14,17 +41,6 @@ int main() {
     }
     imshow("demo1", srcImage);
 //    cout<< srcImage <<endl;
-    waitKey(0);
-    return 0;
-}
-
-#endif
-
-#if 1
-int main(){
-    Mat m = Mat(800,600,CV_8UC3,Scalar(0,0,255));
-    cout<< m <<endl;
-    imshow("testing",m);
     waitKey(0);
     return 0;
 }

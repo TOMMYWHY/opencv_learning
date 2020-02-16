567a1c85cb300862d625215c0b463ac1e8f4bc05
tommy <tommywhy1989@gmail.com>
2020-02-16 23:03:48 +1300

    first blood

--- opencv_01/main.cpp
+++ opencv_01/main.cpp
@@ -9,13 +9,38 @@ using namespace cv;
 
 int main(){
     Mat m = imread("test01.jpeg");
+    Mat_<Vec3d> m2 =m;
+    for (int y = 21; y < 42; y++) {
+        for (int x = 2; x < 21; x++) {
+            if(pow(double(x-11),2) + pow(double(y-31),2)-64.0<0.00001){
+                m2(x,y)=Vec3b(0,0,255);
+            }
+        }
+    }
+
+    if (!m.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("demo4", m2);
+    waitKey(0);
+    return 0;
+
+}
+#endif
+
+
+#if 0
+
+int main(){
+    Mat m = imread("test01.jpeg");
     Vec3i color;
 
     for (int col = 20; col < 140; col++) {
         for (int row = 2; row < 120; row++) {
-            color[0] = (int)(*(m.data + m.step[0]*row + m.step[1]*col));
-            color[1] = (int)(*(m.data + m.step[0]*row + m.step[1]*col+m.elemSize1()));
-            color[2] = (int)(*(m.data + m.step[0]*row + m.step[1]*col+m.elemSize1()*2));
+            color[0] = (int)(*(m.data + m.step[0]*row + m.step[1]*col)); //B
+            color[1] = (int)(*(m.data + m.step[0]*row + m.step[1]*col+m.elemSize1()));//G
+            color[2] = (int)(*(m.data + m.step[0]*row + m.step[1]*col+m.elemSize1()*2));//R
             cout << color[0] <<","<<color[1] << ","<<color[2]<<"==> ";
 
             color[0] = 255;
@@ -35,7 +60,7 @@ int main(){
         std::cout << "Image not loaded";
         return -1;
     }
-    imshow("demo1", m);
+    imshow("demo3", m);
     waitKey(0);
     return 0;
 
@@ -50,7 +75,7 @@ int main(){
         std::cout << "Image not loaded";
         return -1;
     }
-    imshow("demo1", srcImage);
+    imshow("demo2", srcImage);
     waitKey(0);
     return 0;
 
